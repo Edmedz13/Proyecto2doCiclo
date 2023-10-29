d7ce7cdd7afea37070447b08842733cb660d6abb
Edmedz <edmundo.ganoza13@gmail.com>
2023-10-29 02:01:09 -0500

    update diseño Reportes

--- src/menus/HistorialFrame.java
+++ src/menus/HistorialFrame.java
@@ -8,6 +8,9 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Window.Type;
 
 public class HistorialFrame extends JFrame {
 
@@ -17,21 +20,49 @@ public class HistorialFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public HistorialFrame(Object[][] data) {
+		setTitle("\t\t\t\t\t\t\t\t\tREPORTES");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 510, 421);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(222, 184, 135));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		table = new JTable();
+		table.setBackground(new Color(192, 192, 192));
 		table.setModel(new DefaultTableModel(
 			data,
 			new String[] {
-				"Producto", "Cantidad", "Precio"
+				"Producto", "Cantidad", "Precio", "Total"
 			}
 		));
-		table.setBounds(23, 11, 372, 225);
+		table.setBounds(22, 79, 418, 223);
 		contentPane.add(table);
+		
+		JLabel lblNewLabel = new JLabel("Producto");
+		lblNewLabel.setBounds(32, 54, 62, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("PrecioUnitario");
+		lblNewLabel_1.setBounds(128, 54, 103, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Cantidad");
+		lblNewLabel_2.setBounds(254, 54, 62, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("SubTotal");
+		lblNewLabel_3.setBounds(342, 54, 62, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("TotalVentas");
+		lblNewLabel_4.setBounds(44, 328, 103, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("");
+		lblNewLabel_5.setForeground(Color.WHITE);
+		lblNewLabel_5.setBounds(169, 328, 46, 14);
+		contentPane.add(lblNewLabel_5);
 	}
 }
